b349a95294c2458279652101eba1e28bde84ee09
Andrey <opexsomoff@gmail.com>
2020-10-18 21:44:39 +0900

    feat: decided

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const newBracketsConfig = bracketsConfig.map((e) => e.join(""));
+
+    for (let i = 0; i < newBracketsConfig.length; ) {
+        if (str.includes(newBracketsConfig[i])) {
+            str = str.replace(newBracketsConfig[i], "");
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+
+    return str ? false : true;
 }
